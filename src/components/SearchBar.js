587109a887d7164873c0a871c530f168d72e094f
Richard Tae <taerichard@gmail.com>
2022-04-23 13:04:03 -0700

    added styles to searchbar, working on importing magnifying icon to searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,7 +10,10 @@ const SearchBar = () => {
 
 const styles = StyleSheet.create({
     background:{
-        backgroundColor:'#F0EEEE'
+        backgroundColor:'#F0EEEE',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal:15
     }
 })
 
