2ce1fe6e9a0a502fbf6e5c185464253fe92a40f1
Richard Tae <taerichard@gmail.com>
2022-05-03 18:44:55 -0700

    created onTermSubmit callback from searchScreen to searchBar. Changed onChangeText and onEndEditing to reference the callback function from parent

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,16 +4,19 @@ import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
     const [term, setTerm] = useState("");
-    
+
     return (
     <View>
-        <SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)}/>
+        <SearchBar 
+            term={term} 
+            onTermChange={newTerm => setTerm(newTerm)}
+            onTermSubmit={() => console.log("term was submitted")}
+            />
         <Text>Search Screen</Text>
         <Text>{term}</Text>
     </View>)
 }
 
 const styles = StyleSheet.create({})
-
 
 export default SearchScreen;  
