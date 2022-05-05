44077c60bc22ee36b97bf4616aeac55ccf888887
Richard Tae <taerichard@gmail.com>
2022-05-04 21:15:22 -0700

    created yelp API setup on business search

---
+++ src/api/yelp.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: 'https://api.yelp.com/v3/businesses',
+    headers:{
+        Authorization: 'Beaer YFIKJvJ-ojtAPQ4PdsF4F1CsZB_PHaSKwPkF6KQcRtB8E4BUe9T1OxX_3hI-QlV44W4xLNkvBlFa1mv9TGh-ec62LGwcRPkeG_xdun-OIeSMCsFvDjTBpp2mpx76YXYx'
+    }
+});
