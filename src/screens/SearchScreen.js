af29aa5c086472176370e96f0bba908512187d56
Richard Tae <taerichard@gmail.com>
2022-05-04 21:37:52 -0700

    created searchApi function to request yelp API by adding params, response is not successful, working on using async or promises to handle response data

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,42 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet} from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
     const [term, setTerm] = useState("");
+    const [results, setResults] = useState([]); // data from api  
+
+    const searchApi = async () => {
+        const response = await yelp.get('/search', {
+            params:{
+                // attaches to the URL 
+                limit: 50,
+                term, 
+                location: 'seattle' 
+
+            }
+        });
+
+        //error checking 
+        if(response.status !== 201){
+            console.log(response.status)
+            throw new Error("response error")
+        }else{
+            setResults(response.data.businesses);
+        }
+        
+    };
 
     return (
     <View>
         <SearchBar 
             term={term} 
             onTermChange={newTerm => setTerm(newTerm)}
-            onTermSubmit={() => console.log("term was submitted")}
+            onTermSubmit={searchApi}
             />
         <Text>Search Screen</Text>
-        <Text>{term}</Text>
+        <Text>We have found {results.length}</Text>
     </View>)
 }
 
