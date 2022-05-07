0a28a55fea7eb52b9bf5215bd49e298ad6723141
Richard Tae <taerichard@gmail.com>
2022-05-06 21:38:07 -0700

    created a component to show three lists of restaurants and connected to the main searchScreen component, working on displaying data from yelp api for each

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 // allows to run code one time (useEffect) 
 import { View, Text, StyleSheet} from "react-native";
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
     const [term, setTerm] = useState("");
@@ -17,6 +18,9 @@ const SearchScreen = () => {
             />
         {errorMessage ? <Text>{errorMessage}</Text> : null}
         <Text>We have found {results.length}</Text>
+        <ResultsList title="Cost Effective"/>
+        <ResultsList title="Bit Pricier"/>
+        <ResultsList title="Big Spender"/>
     </View>)
 }
 
